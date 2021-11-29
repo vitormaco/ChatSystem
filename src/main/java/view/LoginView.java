049f4a4062f9d3246cc627bc53d6d53778e4ee6a
vitormaco <vitorcmcorrea@gmail.com>
2021-11-29 12:14:43 +0100

    Add login view base

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,32 +1,80 @@
 package view;
-
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginView implements ActionListener {
-    JFrame frame = new JFrame("My window");
-    JButton button = new JButton("Click Me");// Creating object of JButton class
+public class LoginView extends JFrame implements ActionListener {
+
+    Container container = getContentPane();
+    JLabel userLabel = new JLabel("USERNAME");
+    JLabel passwordLabel = new JLabel("PASSWORD");
+    JTextField userTextField = new JTextField();
+    JPasswordField passwordField = new JPasswordField();
+    JButton loginButton = new JButton("LOGIN");
+    JButton resetButton = new JButton("RESET");
+    JCheckBox showPassword = new JCheckBox("Show Password");
 
     public LoginView() {
-        createWindow();
-        createButton();
-    }
 
-    private void createWindow() {
-        frame.getContentPane().setLayout(null);
-        frame.setVisible(true);
-        frame.setBounds(200, 200, 400, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
 
-    public void createButton() {
-        button.setBounds(130, 200, 100, 40);
-        frame.add(button);
-        button.addActionListener(this);// Registering ActionListener to the button
+        this.setTitle("Login Form");
+        this.setVisible(true);
+        this.setBounds(10, 10, 370, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+
+
+        container.setLayout(null);
+
+
+        userLabel.setBounds(50, 150, 100, 30);
+        passwordLabel.setBounds(50, 220, 100, 30);
+        userTextField.setBounds(150, 150, 150, 30);
+        passwordField.setBounds(150, 220, 150, 30);
+        showPassword.setBounds(150, 250, 150, 30);
+        loginButton.setBounds(50, 300, 100, 30);
+        resetButton.setBounds(200, 300, 100, 30);
+
+
+        container.add(userLabel);
+        container.add(passwordLabel);
+        container.add(userTextField);
+        container.add(passwordField);
+        container.add(showPassword);
+        container.add(loginButton);
+        container.add(resetButton);
+
+
+        loginButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        showPassword.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.print("The button was clicked\n");
+        if (e.getSource() == loginButton) {
+            String userText;
+            String pwdText;
+            userText = userTextField.getText();
+            pwdText = passwordField.getText();
+            if (userText.equalsIgnoreCase("admin") && pwdText.equalsIgnoreCase("123")) {
+                JOptionPane.showMessageDialog(this, "Login Successful");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+
+        }
+        if (e.getSource() == resetButton) {
+            userTextField.setText("");
+            passwordField.setText("");
+        }
+        if (e.getSource() == showPassword) {
+            if (showPassword.isSelected()) {
+                passwordField.setEchoChar((char) 0);
+            } else {
+                passwordField.setEchoChar('*');
+            }
+        }
     }
 }
