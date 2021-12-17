3325ecbf1243cdd883e8200833960b38055a5a26
vitormaco <vitorcmcorrea@gmail.com>
2021-12-17 15:59:02 +0100

    Formatting

--- project/src/main/java/view/ChatView.java
+++ project/src/main/java/view/ChatView.java
@@ -67,6 +67,7 @@ public class ChatView extends JFrame implements ActionListener {
 		this.messageService.notifyUserStateChanged("disconnected");
 		this.messageService.disconnectServer();
 	}
+
 	private void handleLogoutButton() {
 		this.logoutProcess();
 		new LoginView(new MessageService());
@@ -95,12 +96,5 @@ public class ChatView extends JFrame implements ActionListener {
 
 	public void updateList(Set<String> list) {
 		this.list.setListData(list.toArray(new String[list.size()]));
-	}
-
-	@Override
-	public void dispose() {
-		this.messageService.notifyUserStateChanged("disconnected");
-		this.messageService.disconnectServer();
-		super.dispose();
 	}
 }
