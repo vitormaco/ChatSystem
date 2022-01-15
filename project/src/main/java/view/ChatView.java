74483ceff55eda7c9acd1a56a11bb621bf7e4472
vitormaco <vitorcmcorrea@gmail.com>
2022-01-15 17:07:59 +0100

    Add broadcast

--- project/src/main/java/view/ChatView.java
+++ project/src/main/java/view/ChatView.java
@@ -43,6 +43,7 @@ public class ChatView extends BaseView implements ActionListener {
 		setActionListeners();
 		setWindowListeners();
 		updateConnectedUsersList();
+		this.messageService.notifyUserStateChanged(MessagePDU.Status.CONNECTION);
 	}
 
 	private void setLogoutButton() {
